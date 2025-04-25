HorizonV2/android_packages_apps_Trebuchet

Launcher3: implement search bar changes
Change-Id: I44d648597daf5298d64aecbdaf8b8bcfbb78f937
Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

src/com/android/launcher3/Utilities.java
@@ -101,6 +101,8 @@ import com.android.launcher3.views.ActivityContext;
 import com.android.launcher3.views.BaseDragLayer;
 import com.android.launcher3.widget.PendingAddShortcutInfo;
 
+import com.android.internal.util.android.Utils;
+
 import java.lang.reflect.Method;
 import java.util.Collections;
 import java.util.List;
@@ -159,6 +161,11 @@ public final class Utilities {
 
     @IntDef({TRANSLATE_UP, TRANSLATE_DOWN, TRANSLATE_LEFT, TRANSLATE_RIGHT})
     public @interface AdjustmentDirection{}
+    
+    public static final String GSA_PACKAGE = "com.google.android.googlequicksearchbox";
+    public static final String LENS_ACTIVITY = "com.google.android.apps.search.lens.LensExportedActivity";
+    public static final String LENS_URI = "google://lens";
+    public static final String PIXEL_SEARCH_PACKAGE = "rk.android.app.pixelsearch";
 
     public static final String DESKTOP_SHOW_QUICKSPACE = "pref_show_quickspace";
     public static final String KEY_SHOW_ALT_QUICKSPACE = "pref_show_alt_quickspace";
@@ -168,6 +175,7 @@ public final class Utilities {
     public static final String KEY_SHOW_QUICKSPACE_WEATHER_CITY = "pref_quickspace_weather_city";
     public static final String KEY_SHOW_QUICKSPACE_WEATHER_TEXT = "pref_quickspace_weather_text";
     public static final String KEY_ALLOW_WALLPAPER_ZOOMING = "pref_allow_wallpaper_zooming";
+    public static final String KEY_HOTSEAT_ENABLED = "pref_hotseat_enabled";
 
     /**
      * Returns true if theme is dark.
@@ -1045,5 +1053,26 @@ public final class Utilities {
     public static boolean canZoomWallpaper(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_ALLOW_WALLPAPER_ZOOMING, true);
+    }
+    
+    public static boolean hotseatEnabled(Context context) {
+        if (isTablet(context)) {
+            return false;
+        }
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_HOTSEAT_ENABLED, true);
+    }
+
+    public static boolean isTablet(Context context) {
+        int smallestWidthDp = context.getResources().getConfiguration().smallestScreenWidthDp;
+        return smallestWidthDp >= 600;
+    }
+    
+    public static boolean isGsaInstalled(Context context) {
+        return Utils.isPackageInstalled(context, GSA_PACKAGE);
+    }
+    
+    public static boolean isPixelSearchInstalled(Context context) {
+        return Utils.isPackageInstalled(context, PIXEL_SEARCH_PACKAGE);
     }
 }

src/com/android/launcher3/qsb/AssistantIconView.kt
@@ -1,0 +1,54 @@
+/*
+ * Copyright (C) 2025 AxionAOSP Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.launcher3.qsb
+
+import android.content.Context
+import android.content.Intent
+import android.util.AttributeSet
+import android.view.View
+import android.widget.ImageView
+
+import com.android.launcher3.R
+import com.android.launcher3.Utilities
+import com.android.internal.util.android.Utils
+
+class AssistantIconView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyle: Int = 0
+) : ImageView(context, attrs, defStyle) {
+
+    private val isGoogleInstalled = Utils.isPackageInstalled(context, Utilities.GSA_PACKAGE)
+
+    init {
+        apply {
+            scaleType = ScaleType.CENTER
+            isFocusable = true
+            setImageResource(R.drawable.ic_mic_color)
+            visibility = if (isGoogleInstalled) VISIBLE else GONE
+            setOnClickListener { launchAssistant() }
+        }
+    }
+
+    private fun launchAssistant() {
+        if (!isGoogleInstalled) return
+        val intent = Intent("android.intent.action.VOICE_ASSIST").apply {
+            putExtra("onesearch_request_type", "TapMicIcon")
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            setPackage(Utilities.GSA_PACKAGE)
+        }
+        runCatching { context.startActivity(intent) }
+    }
+}
+

src/com/android/launcher3/qsb/SearchWidgetView.kt
@@ -1,0 +1,153 @@
+/*
+ * Copyright (C) 2025 AxionAOSP Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.launcher3.qsb
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.res.Configuration
+import android.net.Uri
+import android.util.AttributeSet
+import android.view.ContextThemeWrapper
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.ImageButton
+import android.widget.ImageView
+
+import androidx.core.content.ContextCompat
+
+import com.android.launcher3.R
+import com.android.launcher3.DeviceProfile
+import com.android.launcher3.LauncherPrefChangeListener
+import com.android.launcher3.LauncherPrefs
+import com.android.launcher3.util.ActivityLauncher
+import com.android.launcher3.util.Themes
+import com.android.launcher3.Utilities
+import com.android.launcher3.views.ActivityContext
+
+import com.android.internal.util.android.Utils
+
+class SearchWidgetView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyle: Int = 0
+) : FrameLayout(context, attrs, defStyle), LauncherPrefChangeListener {
+
+    private lateinit var searchIcon: ImageView
+    private lateinit var lensIcon: ImageButton
+    private lateinit var micIcon: ImageView
+    private lateinit var hotseat: View
+
+    private val isGoogleInstalled = Utils.isPackageInstalled(context, Utilities.GSA_PACKAGE)
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+
+        searchIcon = findViewById(R.id.g_icon)
+        lensIcon = findViewById(R.id.lens_icon)
+        micIcon = findViewById(R.id.mic_icon)
+        hotseat = findViewById(R.id.search_container_hotseat)
+
+        lensIcon.visibility = if (isGoogleInstalled) View.VISIBLE else View.GONE
+
+        setClickListeners()
+        LauncherPrefs.Companion.get(context).addListener(this, LauncherPrefs.THEMED_ICONS)
+        updateColors()
+    }
+
+    override fun onPrefChanged(str: String) {
+        if ("themed_icons".equals(str)) {
+            updateColors()
+        }
+    }
+
+    fun updateColors() {
+        val attrColor: Int
+        val style: Int
+        val themedContext: Context
+
+        if (Themes.isThemedIconEnabled(context)) {
+            attrColor = Themes.getAttrColor(context, R.attr.qsbFillColorThemedAllApps)
+            style = R.style.QsbIconTint_Themed
+        } else {
+            attrColor = Themes.getAttrColor(context, R.attr.qsbFillColor)
+            style = R.style.QsbIconTint
+        }
+
+        themedContext = ContextThemeWrapper(context, style)
+
+        hotseat.background?.setTint(attrColor)
+
+        val searchDrawable = ContextCompat.getDrawable(themedContext, if (isGoogleInstalled) R.drawable.ic_super_g_color else R.drawable.ic_allapps_search)!!
+        searchIcon.setImageDrawable(searchDrawable)
+
+        val lensDrawable = ContextCompat.getDrawable(themedContext, R.drawable.ic_lens_color)!!
+        lensIcon.setImageDrawable(lensDrawable)
+
+        val micDrawable = ContextCompat.getDrawable(themedContext, R.drawable.ic_mic_color)!!
+        micIcon.setImageDrawable(micDrawable)
+    }
+
+    override fun onDetachedFromWindow() {
+        LauncherPrefs.Companion.get(context).removeListener(this, LauncherPrefs.THEMED_ICONS)
+    }
+
+    private fun setClickListeners() {
+        setOnClickListener { ActivityLauncher.launchSearch(context) }
+        searchIcon.setOnClickListener { ActivityLauncher.launchSearch(context) }
+        lensIcon.setOnClickListener { ActivityLauncher.launchLensSearch(context) }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val dp = getDp()
+        val hotseatPadding = dp.getHotseatLayoutPadding(context)
+        val availableWidth = dp.availableWidthPx - hotseatPadding.left - hotseatPadding.right
+
+        val horizontalInset = if (dp.isQsbInline) {
+            dp.getAllAppsIconStartMargin(context)
+        } else {
+            val cellWidth = DeviceProfile.calculateCellWidth(
+                availableWidth, dp.hotseatBorderSpace, dp.numShownHotseatIcons
+            )
+            (cellWidth - (dp.iconSizePx * 0.92f)).toInt()
+        }
+
+        val qsbWidth = availableWidth - horizontalInset
+
+        val height = if (Utilities.hotseatEnabled(context)) {
+            MeasureSpec.getSize(heightMeasureSpec)
+        } else {
+            0
+        }
+
+        val childWidthSpec = MeasureSpec.makeMeasureSpec(qsbWidth, MeasureSpec.EXACTLY)
+        val childHeightSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY)
+
+        for (i in 0 until childCount) {
+            getChildAt(i).measure(childWidthSpec, childHeightSpec)
+        }
+
+        setMeasuredDimension(qsbWidth, height)
+    }
+    
+    fun getDp(): DeviceProfile {
+        return (ActivityContext.lookupContext(context) as ActivityContext).getDeviceProfile()
+    }
+
+    fun getTopInset(): Int = if (getDp().isTablet) {
+        resources.getDimensionPixelOffset(R.dimen.qsb_margin_top_adjusting_large)
+    } else {
+        resources.getDimensionPixelSize(R.dimen.qsb_margin_top_adjusting)
+    }
+}

SuperiorOS/packages_apps_Trebuchet

src/com/android/launcher3/Utilities.java
@@ -101,6 +101,8 @@ import com.android.launcher3.views.ActivityContext;
 import com.android.launcher3.views.BaseDragLayer;
 import com.android.launcher3.widget.PendingAddShortcutInfo;
 
+import com.android.internal.util.superior.SuperiorUtils;
+
 import java.lang.reflect.Method;
 import java.util.Collections;
 import java.util.List;
@@ -159,6 +161,11 @@ public final class Utilities {
 
     @IntDef({TRANSLATE_UP, TRANSLATE_DOWN, TRANSLATE_LEFT, TRANSLATE_RIGHT})
     public @interface AdjustmentDirection{}
+    
+    public static final String GSA_PACKAGE = "com.google.android.googlequicksearchbox";
+    public static final String LENS_ACTIVITY = "com.google.android.apps.search.lens.LensExportedActivity";
+    public static final String LENS_URI = "google://lens";
+    public static final String PIXEL_SEARCH_PACKAGE = "rk.android.app.pixelsearch";
 
     public static final String DESKTOP_SHOW_QUICKSPACE = "pref_show_quickspace";
     public static final String KEY_SHOW_ALT_QUICKSPACE = "pref_show_alt_quickspace";
@@ -167,6 +174,7 @@ public final class Utilities {
     public static final String KEY_SHOW_QUICKSPACE_WEATHER = "pref_quickspace_weather";
     public static final String KEY_SHOW_QUICKSPACE_WEATHER_CITY = "pref_quickspace_weather_city";
     public static final String KEY_SHOW_QUICKSPACE_WEATHER_TEXT = "pref_quickspace_weather_text";
+    public static final String KEY_HOTSEAT_ENABLED = "pref_hotseat_enabled";
 
     /**
      * Returns true if theme is dark.
@@ -1039,5 +1047,26 @@ public final class Utilities {
     public static boolean QuickSpaceShowWeatherText(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_SHOW_QUICKSPACE_WEATHER_TEXT, true);
+    }
+
+    public static boolean hotseatEnabled(Context context) {
+        if (isTablet(context)) {
+            return false;
+        }
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_HOTSEAT_ENABLED, true);
+    }
+
+    public static boolean isTablet(Context context) {
+        int smallestWidthDp = context.getResources().getConfiguration().smallestScreenWidthDp;
+        return smallestWidthDp >= 600;
+    }
+
+    public static boolean isGsaInstalled(Context context) {
+        return SuperiorUtils.isPackageInstalled(context, GSA_PACKAGE);
+    }
+
+    public static boolean isPixelSearchInstalled(Context context) {
+        return SuperiorUtils.isPackageInstalled(context, PIXEL_SEARCH_PACKAGE);
     }
 }

src/com/android/launcher3/qsb/AssistantIconView.kt
@@ -1,0 +1,54 @@
+/*
+ * Copyright (C) 2025 AxionAOSP Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.launcher3.qsb
+
+import android.content.Context
+import android.content.Intent
+import android.util.AttributeSet
+import android.view.View
+import android.widget.ImageView
+
+import com.android.launcher3.R
+import com.android.launcher3.Utilities
+import com.android.internal.util.superior.SuperiorUtils
+
+class AssistantIconView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyle: Int = 0
+) : ImageView(context, attrs, defStyle) {
+
+    private val isGoogleInstalled = SuperiorUtils.isPackageInstalled(context, Utilities.GSA_PACKAGE)
+
+    init {
+        apply {
+            scaleType = ScaleType.CENTER
+            isFocusable = true
+            setImageResource(R.drawable.ic_mic_color)
+            visibility = if (isGoogleInstalled) VISIBLE else GONE
+            setOnClickListener { launchAssistant() }
+        }
+    }
+
+    private fun launchAssistant() {
+        if (!isGoogleInstalled) return
+        val intent = Intent("android.intent.action.VOICE_ASSIST").apply {
+            putExtra("onesearch_request_type", "TapMicIcon")
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            setPackage(Utilities.GSA_PACKAGE)
+        }
+        runCatching { context.startActivity(intent) }
+    }
+}
+

src/com/android/launcher3/qsb/SearchWidgetView.kt
@@ -1,0 +1,153 @@
+/*
+ * Copyright (C) 2025 AxionAOSP Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.launcher3.qsb
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.res.Configuration
+import android.net.Uri
+import android.util.AttributeSet
+import android.view.ContextThemeWrapper
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.ImageButton
+import android.widget.ImageView
+
+import androidx.core.content.ContextCompat
+
+import com.android.launcher3.R
+import com.android.launcher3.DeviceProfile
+import com.android.launcher3.LauncherPrefChangeListener
+import com.android.launcher3.LauncherPrefs
+import com.android.launcher3.util.ActivityLauncher
+import com.android.launcher3.util.Themes
+import com.android.launcher3.Utilities
+import com.android.launcher3.views.ActivityContext
+
+import com.android.internal.util.superior.SuperiorUtils
+
+class SearchWidgetView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyle: Int = 0
+) : FrameLayout(context, attrs, defStyle), LauncherPrefChangeListener {
+
+    private lateinit var searchIcon: ImageView
+    private lateinit var lensIcon: ImageButton
+    private lateinit var micIcon: ImageView
+    private lateinit var hotseat: View
+
+    private val isGoogleInstalled = SuperiorUtils.isPackageInstalled(context, Utilities.GSA_PACKAGE)
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+
+        searchIcon = findViewById(R.id.g_icon)
+        lensIcon = findViewById(R.id.lens_icon)
+        micIcon = findViewById(R.id.mic_icon)
+        hotseat = findViewById(R.id.search_container_hotseat)
+
+        lensIcon.visibility = if (isGoogleInstalled) View.VISIBLE else View.GONE
+
+        setClickListeners()
+        LauncherPrefs.Companion.get(context).addListener(this, LauncherPrefs.THEMED_ICONS)
+        updateColors()
+    }
+
+    override fun onPrefChanged(str: String) {
+        if ("themed_icons".equals(str)) {
+            updateColors()
+        }
+    }
+
+    fun updateColors() {
+        val attrColor: Int
+        val style: Int
+        val themedContext: Context
+
+        if (Themes.isThemedIconEnabled(context)) {
+            attrColor = Themes.getAttrColor(context, R.attr.qsbFillColorThemedAllApps)
+            style = R.style.QsbIconTint_Themed
+        } else {
+            attrColor = Themes.getAttrColor(context, R.attr.qsbFillColor)
+            style = R.style.QsbIconTint
+        }
+
+        themedContext = ContextThemeWrapper(context, style)
+
+        hotseat.background?.setTint(attrColor)
+
+        val searchDrawable = ContextCompat.getDrawable(themedContext, if (isGoogleInstalled) R.drawable.ic_super_g_color else R.drawable.ic_allapps_search)!!
+        searchIcon.setImageDrawable(searchDrawable)
+
+        val lensDrawable = ContextCompat.getDrawable(themedContext, R.drawable.ic_lens_color)!!
+        lensIcon.setImageDrawable(lensDrawable)
+
+        val micDrawable = ContextCompat.getDrawable(themedContext, R.drawable.ic_mic_color)!!
+        micIcon.setImageDrawable(micDrawable)
+    }
+
+    override fun onDetachedFromWindow() {
+        LauncherPrefs.Companion.get(context).removeListener(this, LauncherPrefs.THEMED_ICONS)
+    }
+
+    private fun setClickListeners() {
+        setOnClickListener { ActivityLauncher.launchSearch(context) }
+        searchIcon.setOnClickListener { ActivityLauncher.launchSearch(context) }
+        lensIcon.setOnClickListener { ActivityLauncher.launchLensSearch(context) }
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val dp = getDp()
+        val hotseatPadding = dp.getHotseatLayoutPadding(context)
+        val availableWidth = dp.availableWidthPx - hotseatPadding.left - hotseatPadding.right
+
+        val horizontalInset = if (dp.isQsbInline) {
+            dp.getAllAppsIconStartMargin(context)
+        } else {
+            val cellWidth = DeviceProfile.calculateCellWidth(
+                availableWidth, dp.hotseatBorderSpace, dp.numShownHotseatIcons
+            )
+            (cellWidth - (dp.iconSizePx * 0.92f)).toInt()
+        }
+
+        val qsbWidth = availableWidth - horizontalInset
+
+        val height = if (Utilities.hotseatEnabled(context)) {
+            MeasureSpec.getSize(heightMeasureSpec)
+        } else {
+            0
+        }
+
+        val childWidthSpec = MeasureSpec.makeMeasureSpec(qsbWidth, MeasureSpec.EXACTLY)
+        val childHeightSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY)
+
+        for (i in 0 until childCount) {
+            getChildAt(i).measure(childWidthSpec, childHeightSpec)
+        }
+
+        setMeasuredDimension(qsbWidth, height)
+    }
+    
+    fun getDp(): DeviceProfile {
+        return (ActivityContext.lookupContext(context) as ActivityContext).getDeviceProfile()
+    }
+
+    fun getTopInset(): Int = if (getDp().isTablet) {
+        resources.getDimensionPixelOffset(R.dimen.qsb_margin_top_adjusting_large)
+    } else {
+        resources.getDimensionPixelSize(R.dimen.qsb_margin_top_adjusting)
+    }
+}