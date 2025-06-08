92df32c2e56bf845a16646d2f065d544c1cc9ef1
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-06-08 16:52:33 +0800

    RecentsView: fix lock feature conflicts with split screen
    Change-Id: I676317c1af44850744ece3cd55795e9a8af4d8d4
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -3678,7 +3678,7 @@ public abstract class RecentsView<
             boolean animateTaskView, boolean shouldRemoveTask, long duration,
             boolean dismissingForSplitSelection) {
         boolean isTaskLocked = mLockedTasks.contains(dismissedTaskView.getFirstTask().key.getPackageName());
-        if (isTaskLocked) return;
+        if (isTaskLocked && !dismissingForSplitSelection) return;
         if (mPendingAnimation != null) {
             mPendingAnimation.createPlaybackController().dispatchOnCancel().dispatchOnEnd();
         }
